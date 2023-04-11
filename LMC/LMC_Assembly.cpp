49da645ea1da199f4ce9dcdd3e2c534aa1fb863a
natliaost <128471889+natliaost@users.noreply.github.com>
2023-04-11 11:24:56 +0300

    throws and exception library

--- LMC/LMC_Assembly.cpp
+++ LMC/LMC_Assembly.cpp
@@ -6,6 +6,7 @@
 #include "from_trusted_Symbols.h"
 #include "Commands.h"
 #include "SymbolsValidator.h"
+#include "Exceptions.h"
 
 namespace experis
 {
@@ -14,20 +15,11 @@ LMC_Assembly::LMC_Assembly(const int a_argc, const char **a_argv)
 	: m_argc{a_argc}
 	, m_argv{a_argv}
 {
-	if(!IsValidArguments()) 
+	ArgumentsValidation();
+	InputCodeFileExistenceCheck();
+	if(!IsValidSymbols(Commands(GetInputCodeFilePath())))
 	{
-		// THROW!!
-	}
-
-	if(!IsCodeFileExists())
-	{
-		//THROW!!
-	}
-
-	const std::string codeFilePath = *(this->m_argv + 1);	
-	if(!IsValidSymbols(Commands(codeFilePath)))
-	{
-		//THROW!!
+		throw SymbolsException{};
 	}
 
 	//if(false)//!valid machine code <==================================RENANA
@@ -46,98 +38,128 @@ LMC_Assembly::~LMC_Assembly()
 
 void LMC_Assembly::trusted_CreateSymbolFile() const
 {
-	const std::string inputCodeFilePath = *(this->m_argv + 1);
+	const std::string inputCodeFilePath = GetInputCodeFilePath();
+	const std::string outputSymbolFilePath = GetOutputSymbolFilePath();
 
-	std::string outputSymbolFilePath{};
-	switch (this->m_argc)
-	{
-	case 2 || 3:
-	{
-		outputSymbolFilePath = ChangeExtention(inputCodeFilePath, ".sym");
-	}
-	case 4:
-	{
-		outputSymbolFilePath = *(this->m_argv + 3);
-	}
-	case 5:
-	{
-		outputSymbolFilePath = *(this->m_argv + 4);
-	}
-	default:
-		assert(true); 
-	}
 	SymbolsToFile(outputSymbolFilePath, from_trusted_Symbols(Commands(inputCodeFilePath)));
 }
 
 //void LMC_Assembly::CreateMachineCodeFile() const //<=====================================RENANA
 //{
-//  const std::string inputCodeFilePath = *(this->m_argv + 1);
-//
-//	std::string outputMachineCodeFilePath{};
+//  const std::string inputCodeFilePath = GetInputCodeFilePath();
+//	const std::string outputMachinCodeFilePath = GetOutputMachinCodeFilePath();
+// 
 // 	switch (this->m_argc) 
 //	{
-//	case 2:
+//	case 2 || 4:
 //	{
-//		outputMachineCodeFilePath = ChangeExtention(inputCodeFilePath, ".lmc");
-//		// create TEXT file with the name outputMachineCodeFilePath
+//		// create from inputCodeFilePath a TEXT file with the name outputMachinCodeFilePath // <=====RENENA
 //	}
-//	case 3:
+//	case 3 || 5:
 //	{
-//		outputMachineCodeFilePath = ChangeExtention(inputCodeFilePath, ".bin");
-//		// create BIN file with the name outputMachineCodeFilePath
-//	}
-//	case 4: 
-//	{
-//		outputMachineCodeFilePath = *(this->m_argv + 3);
-//		// create TEXT file with the name outputMachineCodeFilePath
-//	}
-// case 5: 
-//	{
-//		outputMachineCodeFilePath = *(this->m_argv + 4);
-//		// create BIN file with the name outputMachineCodeFilePath
+//		// create from inputCodeFilePath a BIN file with the name outputMachinCodeFilePath // <=====RENENA
 //	}
 //	default:
-//		assert(); //programming bug. should not get here because it was checked in the constructor
+//		assert(true); //programming bug. should not get here because it was checked in the constructor
 //	}
-// 
 //}
 
-const bool LMC_Assembly::IsValidArguments() const
+const std::string LMC_Assembly::GetInputCodeFilePath() const
+{
+	return *(this->m_argv + 1);
+}
+
+const std::string LMC_Assembly::GetOutputSymbolFilePath() const
+{
+	const std::string inputCodeFilePath = GetInputCodeFilePath();
+
+	switch (this->m_argc)
+	{
+	case 2 || 3:
+	{
+		return ChangeExtention(inputCodeFilePath, ".sym");
+	}
+	case 4:
+	{
+		return *(this->m_argv + 3);
+	}
+	case 5:
+	{
+		return *(this->m_argv + 4);
+	}
+	default:
+		assert(true); 
+	}
+}
+
+const std::string LMC_Assembly::GetOutputMachinCodeFilePath() const
+{
+	assert(this->m_argc == 2 || this->m_argc == 3 || this->m_argc == 4 || this->m_argc == 5);
+
+	const std::string inputCodeFilePath = GetInputCodeFilePath();
+
+	switch (this->m_argc) 
+	{
+	case 2:
+	{
+		return ChangeExtention(inputCodeFilePath, ".lmc");
+	}
+	case 3:
+	{
+		return ChangeExtention(inputCodeFilePath, ".bin");
+	}
+	case 4: 
+	{
+		return *(this->m_argv + 3);
+	}
+	case 5: 
+	{
+		return *(this->m_argv + 4);
+	}
+	default:
+	{
+		assert(true);
+	}
+	}
+}
+
+void LMC_Assembly::ArgumentsValidation() 
 {
 	switch (this->m_argc)
 	{
 	case 2:
 	{
-		return true;
+		break;
 	}
-	case 3:
+	case 3 || 5:
 	{
-		return *(this->m_argv + 2) == std::string("/bin");
+		if (*(this->m_argv + 2) != std::string("/bin"))
+		{
+			MainArgumentsException error{"Second input argument is not \"/bin\" !!! "};
+			throw error;
+		}
 	}
 	case 4:
 	{
-		return true;
-	}
-	case 5:
-	{
-		return *(this->m_argv + 2) == std::string("/bin");
+		break;
 	}
 	default:
-		return false;
+		MainArgumentsException error{"Invalid number of input arguments !!! "};
+		throw error;
 	}
 }
 
-const bool LMC_Assembly::IsCodeFileExists() const
+void LMC_Assembly::InputCodeFileExistenceCheck()
 {
    std::ifstream infile;
    infile.open(*(this->m_argv + 2));
    if(infile.is_open()) 
    {
 	   infile.close();
-       return true;
-   } else 
+   } 
+   else 
    {
-       return false;
+		throw InputFileOpeningException{};
    }
 }
 
@@ -146,3 +168,47 @@ const bool LMC_Assembly::IsCodeFileExists() const
 
 
 
+
+
+
+
+
+//const bool LMC_Assembly::IsValidArguments() const
+//{
+//	switch (this->m_argc)
+//	{
+//	case 2:
+//	{
+//		return true;
+//	}
+//	case 3:
+//	{
+//		return *(this->m_argv + 2) == std::string("/bin");
+//
+//	}
+//	case 4:
+//	{
+//		return true;
+//	}
+//	case 5:
+//	{
+//		return *(this->m_argv + 2) == std::string("/bin");
+//	}
+//	default:
+//		return false;
+//	}
+//}
+
+//const bool LMC_Assembly::IsCodeFileExists() const
+//{
+//   std::ifstream infile;
+//   infile.open(*(this->m_argv + 2));
+//   if(infile.is_open()) 
+//   {
+//	   infile.close();
+//       return true;
+//   } else 
+//   {
+//       return false;
+//   }
+//}
